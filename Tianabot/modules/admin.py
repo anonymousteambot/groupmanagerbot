95197e4814de561659049272bae293b441e54b7d
anonymousteambot <103872671+anonymousteambot@users.noreply.github.com>
2022-04-22 20:38:01 +0530

    Update admin.py

--- Tianabot/modules/admin.py
+++ Tianabot/modules/admin.py
@@ -708,7 +708,7 @@ dispatcher.add_handler(SETCHAT_TITLE_HANDLER)
 dispatcher.add_handler(SETSTICKET_HANDLER)
 dispatcher.add_handler(SETDESC_HANDLER)
 
-__mod_name__ = "Aᴅᴍɪɴ"
+__mod_name__ = "𝓐𝓓𝓜𝓘𝓝"
 __command_list__ = [
     "adminlist",
     "admins",
